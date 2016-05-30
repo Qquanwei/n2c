80b6b0526cde9bb6fdf9bb544dcb17df8cbae3ec
quanwei <quanwei9958@126.com>
2016-05-30 16:56:58 +0800

    init n2c

---
+++ index.js
@@ -0,0 +1,27 @@
+"use strict"
+function n2c(num){
+  let convert = { '0':'零', '1':'一', '2':'二', '3':'三', '4':'四', '5':'五', '6':'六', '7':'七', '8':'八', '9':'九','.':'' };
+  let weight= {
+    '0':'',
+    '1':'十',
+    '2':'百',
+    '3':'千',
+    '4':'万',
+    '8':'亿',
+  };
+  let an=[];
+  num=num+'';
+  num=(num+'').replace(/^0+/,'').replace(/0{2,}/,function(w){return '.'.repeat(w.length)}).replace(/0$/,'.')
+  let length=num.length;
+  function getWeight(index,only){
+    if(index<4) return (!only)?weight[index]:'';
+    else if(index<8) return getWeight(index-4,only)+(index==4?'万':'');
+    else return getWeight(index-8,only)+ ((index%8==0)?'亿':'');
+  }
+  for(let i=0;i<length;++i) an.push(num[i]);
+  return an.map(function(iter,index){
+    return convert[iter]+(iter!=='0'?getWeight(length-index-1,iter==='.'):'');
+  }).join('');
+}
+
+module.exports=n2c;
---
+++ test/test.js
@@ -0,0 +1,17 @@
+var n2c = require('../index.js');
+var assert = require('chai').assert;
+describe('n2c',function(){
+  it('123 convert 一百二十三',function(){
+    assert(n2c('123')==='一百二十三');
+  });
+
+  it('lang number test',function(){
+    assert(n2c('123456789009876543210')==='一万二千三百四十五亿亿六千七百八十九万九十八亿七千六百五十四万三千二百一十');
+  });
+
+  it('zero test',function(){
+    assert(n2c('1000')==='一千')
+    assert(n2c('001000201')==='一百万二百零一');
+  });
+
+});
